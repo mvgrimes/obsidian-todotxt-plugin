02900c761dda9838a974fa6be593305bb3a62e34
Mark Grimes <mgrimes@cpan.org>
2022-12-13 21:27:09 -0500

    chore: convert to tailwindcss

--- src/ui/todolist.tsx
+++ src/ui/todolist.tsx
@@ -16,43 +16,65 @@ export const TodoList = (props: TodoListProps) => {
   };
 
   return (
-    <div className="todos">
+    <div>
       {sorted?.map((todo) => (
         <div
           className={cn(
-            'todo',
-            todo.priority === 'A'
-              ? 'todo-priority-a'
-              : todo.priority === 'B'
-              ? 'todo-priority-b'
-              : todo.priority === 'C'
-              ? 'todo-priority-c'
-              : '',
+            'my-2 mx-2',
             todo.completed ? 'todo-completed' : 'todo-not-completed',
           )}
           key={todo.id}
         >
-          <label htmlFor={`todo-${todo.id}`}>
-            <input
-              type="checkbox"
-              checked={todo.completed}
-              id={`todo-${todo.id}`}
-              onChange={() => handleChange(todo.id)}
-            />
-            <span className="todo-priority">{todo.priority}</span>
-            {/* <span className="todo-completedDate">{todo.completedDate}</span> */}
-            {/* <span className="todo-createdDate">{todo.createDate}</span> */}
+          <label htmlFor={`todo-${todo.id}`} className="flex items-start">
+            <div className="mt-0.5">
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                id={`todo-${todo.id}`}
+                onChange={() => handleChange(todo.id)}
+              />
+            </div>
+            <span
+              className={cn(
+                'mr-1 rounded-lg my-0.5 px-1.5 py-0.5 text-xs',
+                todo.priority === 'A'
+                  ? 'text-red-700 bg-red-300'
+                  : todo.priority === 'B'
+                  ? 'text-amber-600 bg-orange-300'
+                  : todo.priority === 'C'
+                  ? 'text-sky-600 bg-blue-300'
+                  : '',
+              )}
+            >
+              {todo.priority}
+            </span>
+            {/* <span>{todo.completedDate}</span> */}
+            {/* <span>{todo.createDate}</span> */}
             <span>
-              <span className="todo-description">{todo.description}</span>
+              <span
+                className={cn(
+                  todo.completed
+                    ? 'line-through text-gray-400'
+                    : 'text-gray-200',
+                )}
+              >
+                {todo.description}
+              </span>
               {todo.tags
                 .filter((tag) => tag !== props.tag)
                 .map((tag) => (
-                  <span className="todo-tag" key={tag}>
+                  <span
+                    className="text-sm ml-2 rounded-full py-0.5 px-1 text-gray-700 bg-blue-200"
+                    key={tag}
+                  >
                     {tag}
                   </span>
                 ))}
               {todo.ctx.map((ctx) => (
-                <span className="todo-ctx" key={ctx}>
+                <span
+                  className="text-xs ml-2 rounded-full py-0.5 px-1 text-gray-700 bg-indigo-200"
+                  key={ctx}
+                >
                   {ctx}
                 </span>
               ))}
